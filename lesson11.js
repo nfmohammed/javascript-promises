f26dbe4e4828dc4f897d77decbf69ed1d0b8bab4
Naseer Mohammed <nmohammed@expedia.com>
2020-06-07 16:31:56 -0500

    lesson12

--- lesson11.js
+++ lesson11.js
@@ -2,14 +2,30 @@
 const fetch = require("node-fetch");
 
 //execute fetch over the network.
-const promise = fetch('https://jsonplaceholder.typicode.com/todos/1');
+const promise1 = fetch('https://jsonplaceholder.typicode.com/todos/1');
 
 //then, catch execution will wait for the promise to resolve. 
 //then blocks can be chained
-promise
+promise1
     .then(res => res.json())
-    .then(user => console.log('user.title = ', user.title ))
+    .then(user => console.log('promise1 user.title = ', user.title ))
     .catch(err => console.error(err));
 
 //this is executed first in the same event loop
-console.log("Synchronous...");
+console.log("promise1 scheduled...");
+
+
+const promise2 = fetch('https://jsonplaceholder.typicode.com/todos/-1');
+
+promise2
+    .then(res => res.json())
+    .then(user => {
+        if(user.title === undefined) {
+            //note: throw the error, do not return the error
+            throw new Error("promise2 user undefined");
+        }
+        return user;
+    })
+    .catch(err => console.error(err));
+
+console.log("promise2 scheduled...");
---
+++ lesson12.js
@@ -0,0 +1,41 @@
+const tick = Date.now();
+const log = (v) => console.log(`${v} - Elapsed: ${Date.now() - tick}ms`);
+
+const codeBlocker = () => {
+    console.log("executing codeblocker...");
+   
+    // Blocking
+    // let i = 0;
+    // while(i < 1000000000) { i++;}
+
+    // return 'billion loops done';
+
+
+    // Async blocking
+
+    // return new Promise((resolve, reject) => {
+
+    //     let i = 0;
+    //     while(i < 1000000000) { i++;}
+ 
+    //     resolve('billion loops done');
+    // })
+
+
+    // Non-blocking
+
+    return Promise.resolve().then(v =>  {
+        let i = 0;
+        while(i < 1000000000) { i++; }
+        return 'billion loops done';
+    })
+
+}
+
+log('Synchronous 1');
+
+//codeBlocker();
+codeBlocker().then(log)
+
+
+log('Synchronous 2');
